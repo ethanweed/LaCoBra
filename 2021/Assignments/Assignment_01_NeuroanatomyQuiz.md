a4ad2fdf38428d89462be246e692ac83a245248f
Ethan Weed <weed.ethan@gmail.com>
2021-08-26 10:48:39 +0200

    auto updated from desktop

---
+++ 2021/Assignments/Assignment_01_NeuroanatomyQuiz.md
@@ -0,0 +1,11 @@
+The readings on brain anatomy can be quite challenging, so we need to help each other out. In your groups, make at least 5 questions to challenge your classmates, based on Kemmerer pp. 3-10. 
+
+Make these in the form of multiple choice questions, with plausible alternative answers. E.g. “where in the brain is Broca’s area located?” a) right parietal lobe, b) left occipital lobe, c) left temporal lobe, \*d) left frontal lobe. 
+
+Make sure you indicate the correct answer. 
+
+I will select from your questions and make an in-class quiz for Thursday, so please get your answers to me as soon as you can (Monday evening at the latest).
+
+Please submit only one form per group.
+
+You can find the form here:  <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVCcRwYWOxmWPxrZ14WY2O6MEPs9rtC4a-vyv9zqT5D3RYdg/viewform?usp=sf_link" target="_blank">Quiz questions Week 1</a>
--- build.sh
+++ build.sh
@@ -5,16 +5,27 @@ pandoc --standalone /Users/ethan/Documents/GitHub/LaCoBra/2021/Syllabus.md -o /U
 
 pandoc --standalone /Users/ethan/Documents/GitHub/LaCoBra/2021/Schedule.md -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Schedule.html
 
+# Assignments
+pandoc --standalone /Users/ethan/Documents/GitHub/LaCoBra/2021/Assignments/Assignment_01_NeuroanatomyQuiz.md -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Assignments/Assignment_01_NeuroanatomyQuiz.html
+
+# ---------------------------------------- 
+
 # Slides
 
+## Lecture Slides
+
 pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_01_Introduction.html /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_01_Introduction.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
+
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_02_Aphasia1.html /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_02_Aphasia1.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
+
+## Lab Slides
 
 pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lab_01_Stroop.html /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lab_01_Stroop.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 
 
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_02_Aphasia1.html /Users/ethan/Documents/GitHub/LaCoBra/2021/Slides/Lecture_02_Aphasia1.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 
 
+# ---------------------------------------- 
 
 # Push to github
 
